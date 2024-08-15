60ba033304cf455bf91d8f86ee66747161054f57
Subat Ahsan <subat.ahsan18@gmail.com>
2024-08-14 21:20:51 -0400

    Created program

---
+++ README.md
@@ -0,0 +1,48 @@
+# Pdf Table of Content Creator
+
+**Tech Used:** Python
+
+## Description
+
+CLI application used to to create or modify the table of content (outline) of a pdf. Uses pymupdf to edit pdf. 
+
+## How to Use
+
+Run the command with the pdf file path as well as the input_data file path. 
+
+```
+python TOC.py [pdf_file_path.pdf] -i [input_file_path.txt] [other options]
+```
+
+**Input File Format**
+
+Input file needs to be a .txt file with each line defining a heading for the outline. Use `>`s to indicate the heading level, follow it with the name of the heading. Place a comma and the page number after. `>Name,PageNum`
+
+E.G:
+
+```
+>Heading 1, 1
+>>Sub-Heading 1, 2
+>>Sub-Heading 2, 20
+>>>Sub-Sub-Heading 1, 45
+>Heading 2, 60
+```
+
+Lines that do not start with '>' or that contain invalid data will be ignored. The headings also get auto sorted, so if headings with lower page numbers appear later, they will be placed in the correct place.
+
+
+
+**Options**
+
+| Option        | Description                                                                                                                                                                                       |
+| ------------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| file_path     | The first and only positional arugment. Defines the name of the pdf to use.                                                                                                                       |
+| --input, -i   | Defines the input data file to use to input outline headings from. Also where headings are outputted to when using display mode option. **[Required]**                                            |
+| --offset, -s  | The amount of offset the heading placement by. E.G: If a heading is supposed to be on page 1, but is on page 21 of the pdf, set `-s 20`, and then input 1 for the heading in the 'input_data.txt' |
+| --remove, -r  | Add `-r` flag to command to clear previous table of contents in pdf and only have inputted one.                                                                                                   |
+| --output, -o  | Output location of file. If not set, overwrites the input file when saving pdf.                                                                                                                   |
+| --display, -d | Stores the pdf's current table of content in the file defined by `-i`. Does not use offset, remove, or output options                                                                             |
+
+**Full Example:** `python TOC.py input.pdf -o output.pdf -s 30 -i a.txt -r`
+
+
---
+++ TOC.py
@@ -0,0 +1,92 @@
+import pymupdf
+import argparse
+import os
+
+def validateFiles(filename, input_file, check_input):
+    if not os.path.exists(filename):
+        print("PDF File does not exist")
+        return False
+    if not filename.lower().endswith(".pdf"):
+        print("PDF file must be pdf")
+        return False
+      
+    if check_input and not os.path.exists(input_file):
+        print("Input file does not exist")
+        return False
+    
+    return True
+        
+def updatePdfToc(filename, input_file, shift, remove, output):
+    with open(filename, 'rb' ) as file:
+        print("Running\n")
+        pdf_reader = pymupdf.open(file)
+        
+        toc = []
+        if not remove:
+             toc += pdf_reader.get_toc()
+        
+        
+        with open(input_file, 'r' , encoding='utf-8') as input_data:
+            for line in input_data:
+                count = 0
+                for c in line:
+                    if (c == '>'):
+                        count += 1
+                    else:
+                        break
+                
+                if count == 0: 
+                    continue
+                    
+                remainingLine = line[count:].lstrip().rstrip()
+                remainingLineParts = remainingLine.split(',')
+                
+                toc.append([count,remainingLineParts[0].lstrip().rstrip()
+                ,int(remainingLineParts[1])+shift])
+        
+        
+        sortedToc = sorted(toc, key= lambda val : val[2])
+        pdf_reader.set_toc(sortedToc)    
+
+        if (output == None):
+            pdf_reader.save(filename, incremental=True, encryption=pymupdf.PDF_ENCRYPT_KEEP)
+        elif (output == filename):
+            pdf_reader.save(output, incremental=True, encryption=pymupdf.PDF_ENCRYPT_KEEP)
+        else:
+            pdf_reader.save(output)
+        
+    print("\nFinished")
+    
+def writeInputData(filename, output_file):
+    pdf_reader = pymupdf.open(filename) 
+    with open(output_file, 'w' , encoding='utf-8') as file:
+        toc = pdf_reader.get_toc()
+        for i in toc:
+            outline_string = '>'*i[0] + i[1]+','+str(i[2]) +'\n'
+            file.write(outline_string)
+            
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(
+         prog='TOC Creator',
+         description='Create table of contents for pdf files',
+        )
+    parser.add_argument('file_path', help="Path for pdf file")    
+    parser.add_argument('-i', '--input', required=True, 
+        help="Path for input table of contents data file")
+    parser.add_argument('-s', '--offset', 
+        help="Amount to offset page numbers by", default = 0, type=int)
+    parser.add_argument('-r', '--remove', action="store_true",
+        help = "Removes previous table of content")
+    parser.add_argument('-o', '--output', 
+        help="Output file location. Overwrites input file if not set")
+    parser.add_argument('-d', '--display', action="store_true", 
+        help="""Stores current table of contents as input data in the file specified by
+        -i. Does not update TOC.""")
+
+    args = parser.parse_args()
+    if (validateFiles(args.file_path, args.input, not args.display)):
+        if (args.display):
+            writeInputData(args.file_path, args.input)
+        else:
+            updatePdfToc(args.file_path, args.input, args.offset, args.remove,args.output)
